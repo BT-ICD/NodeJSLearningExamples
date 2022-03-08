8bed3ebc8ebfe945390ea175023c9b26563dc779
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-03-08 20:37:22 +0530

    __filename to get current file name example

--- readfiledemo.js
+++ readfiledemo.js
@@ -2,9 +2,14 @@ const fs = require('fs');
 const fileName = '1-use.js';
 
 // Use of Immediately-invoked Function Expression 
-(function readFileContent(){
-    const data = fs.readFileSync(fileName,'UTF-8');
-    console.log(data);
-})()
+// (function readFileContent(){
+//     const data = fs.readFileSync(fileName,'UTF-8');
+//     console.log(data);
+// })()
 // const data = fs.readFileSync(fileName,'UTF-8');
 //     console.log(data);
+
+const data = fs.readFileSync(__filename, 'utf-8');
+let fileName1 = __filename;
+console.log(`Filename 1 is ${fileName1} \n`);
+console.log('Content of this file \n',data);
