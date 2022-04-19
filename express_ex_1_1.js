b79821baacc1cce1b7d377799f440613298e5113
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-04-19 19:48:59 +0530

    First example of express

---
+++ express_ex_1_1.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+app.listen(PORT,function(){
+    console.log(`Example app listening on port ${PORT}!`);
+});
